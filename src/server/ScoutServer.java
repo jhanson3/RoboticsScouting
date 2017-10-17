ad0ec802cb0541393486114a66dcbfd7c98b30b8
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2017-10-17 11:49:55 -0700

    Added socket in the server

--- src/server/ScoutServer.java
+++ src/server/ScoutServer.java
@@ -1,10 +1,22 @@
 package server;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 
 public class ScoutServer {
 
 	public static void main(String[] args) throws IOException{
 		
+		// Create Socket
+		ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(1011);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+        
+        
+        
 	}
 }
