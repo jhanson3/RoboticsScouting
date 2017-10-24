88eabb9952e73818f52fd6c4ef6849ad2bfae196
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2017-10-24 14:36:56 -0700

    Created getInstanceOf and addTeam to TeamSheet.java

--- src/serverDataBase/TeamSheet.java
+++ src/serverDataBase/TeamSheet.java
@@ -15,7 +15,8 @@ public class TeamSheet implements Serializable {
 
 	private static final long serialVersionUID = -215213122891539175L;
 	
-	HashMap<Integer, Team> teams;
+	private HashMap<Integer, Team> teams;
+	private TeamSheet teamSheet;
 	
 	/*
 	 * Constructor
@@ -25,7 +26,53 @@ public class TeamSheet implements Serializable {
 	 * Parameters: None
 	 */
 	private TeamSheet() {
+		teamSheet = this;
 		teams = new HashMap<Integer, Team>();
+	}
+	
+	/*
+	 * getInstanceOf
+	 * Author: Jeremiah Hanson
+	 * ---------------------------------------------
+	 * Purpose: this is how to retrieve the only 
+	 * 		instance of this class. If its the first
+	 * 		call it creates an instance using the 
+	 * 		constructor.
+	 * Parameters:
+	 * 	None
+	 */
+	public TeamSheet getInstanceOf() {
+		
+		if (teamSheet == null) {
+			teamSheet = new TeamSheet();
+		}
+		
+		return teamSheet;
+	}
+	
+	/*
+	 * addTeam
+	 * Author: Jeremiah Hanson
+	 * --------------------------------------------
+	 * Purpose: creates a new team and adds it to the
+	 * 		hashmap with its team number as a key
+	 * Parameters:
+	 * 	num: the team number
+	 * 	hasName: boolean value tells if the name was \
+	 * 		entered when creating
+	 * 	name: the name of the team as a String
+	 */
+	public void addTeam(int num, boolean hasName, String name) {
+		
+		Team team;
+		
+		if (hasName) {
+			team = new Team(num, name);
+		} else {
+			team = new Team(num);
+		}
+		
+		teams.put(team.getTeamNum(), team);
 	}
 	
 }
