aaf83758c9882a70fc7594f6ee6c4cfe83f02a08
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2017-10-24 15:03:16 -0700

    Added MatchList.java as a singleton

---
+++ src/serverDataBase/MatchList.java
@@ -0,0 +1,81 @@
+package serverDataBase;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MatchList implements Serializable{
+
+	private static final long serialVersionUID = -2479177965817464578L;
+	
+	private MatchList matchList;
+	private ArrayList<Match> matches;
+	
+	/*
+	 * Constructor
+	 * Author: Jeremiah Hanson
+	 * -------------------------------------------
+	 * Purpose: Constructor
+	 */
+	private MatchList() {
+		matchList = this;
+		matches = new ArrayList<Match>();
+	}
+	
+	/*
+	 * getInstanceOf
+	 * Author: Jeremiah Hanson
+	 * --------------------------------------------
+	 * Purpose: Gets the instance of MatchList, or 
+	 * 		creates it if none exists.
+	 */
+	public MatchList getInstanceOf() {
+		
+		if (matchList == null) {
+			matchList = new MatchList();
+		}
+		
+		return matchList;
+	}
+	
+	/*
+	 * generateMatchList
+	 * Author: Jeremiah Hanson
+	 * ----------------------------------------------
+	 * Purpose: Creates a given number of Matches and
+	 * 		adds them to the ArrayList matches.
+	 * Parameters:
+	 * 	num: the number of matches to add
+	 */
+	public void generateMatchList(int num) {
+		
+		for (int i=0; i < num; i++) {
+			Match temp = new Match(i);
+			matches.add(temp);
+		}
+	}
+	
+	/*
+	 * addMatch
+	 * Author: Jeremiah Hanson
+	 * -----------------------------------------------
+	 * Purpose: Adds a single match to the end of the 
+	 * 		MatchList.
+	 */
+	public void addMatch() {
+		
+		matches.add(new Match(matches.size()));
+	}
+	
+	/*
+	 * getMatch
+	 * Author: Jeremiah Hanson
+	 * ------------------------------------------------
+	 * Purpose: gets a specific match number
+	 * Parameters: 
+	 * 	num: the match number to get
+	 */
+	public Match getMatch(int num) {
+		return matches.get(num);
+	}
+
+}
