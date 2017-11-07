f7b4a1b180b250b22ea5e020f6781e112004b2e6
Jeremiah Hanson <hanson.jeremiah@comcast.net>
2017-11-07 15:14:17 -0700

    Added a SetupView

--- src/client/ClientGUI.java
+++ src/client/ClientGUI.java
@@ -8,19 +8,24 @@
 
 package client;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ClientGUI extends JFrame{
 
 	private static final long serialVersionUID = -8877474517268367965L;
+	private JPanel left, right;
 
 	public ClientGUI() {
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(1000, 600);
+		this.setSize(2000, 1000);
 		this.setLocation(100, 100);
 		this.setTitle("CRUSH Scouting");
-		this.setLayout(null);
+		this.setLayout(new GridLayout(2, 1));
+		
 	}
 	
 }
